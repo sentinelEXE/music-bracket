e4bdf15bf7af1bbf97d3cdcc49b3cfea4dfd46cd
sentinelEXE <sngfshr@gmail.com>
2024-11-08 22:20:27 -0600

    Fix song randomizer

--- src/components/BracketPage.tsx
+++ src/components/BracketPage.tsx
@@ -1,44 +1,53 @@
 // src/components/BracketPage.tsx
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Playlist, Song } from '../types/types';
 import fetchPlaylistSongs from '../api/fetch-playlist-songs';
+import { getRandomSongs } from '../utils/get-random-songs';
+import { setSongs } from '../store/store';
 
 export const BracketPage: React.FC = () => {
-  const selectedPlaylist = useSelector((state: any) => state.selectedPlaylist) as Playlist;
-  const contestantNumber = useSelector((state: any) => state.contestantNumber);
-  const [songs, setSongs] = useState<Song[]>([]);
-  const accessToken = localStorage.getItem('access_token');
+    const dispatch = useDispatch();
+    const selectedPlaylist = useSelector((state: any) => state.selectedPlaylist) as Playlist;
+    const contestantNumber = useSelector((state: any) => state.contestantNumber);
+    const [songs, setSongsState] = useState<Song[]>([]);
+    const [songsRandomized, setSongsRandomized] = useState<boolean>(false);
+    const accessToken = localStorage.getItem('access_token');
 
-  useEffect(() => {
-    if (selectedPlaylist && accessToken) {
-      fetchPlaylistSongs(selectedPlaylist.id, accessToken)
-        .then(allTracks => setSongs(allTracks))
-        .catch(error => console.error('Error fetching playlist details:', error));
-    }
-  }, [selectedPlaylist, accessToken]);
+    useEffect(() => {
+        if (selectedPlaylist && accessToken) {
+        fetchPlaylistSongs(selectedPlaylist.id, accessToken)
+            .then(allTracks => setSongsState(allTracks))
+            .catch(error => console.error('Error fetching playlist details:', error));
+        }
+    }, [selectedPlaylist, accessToken]);
 
-  useEffect(() => {
-    console.log({ songs: songs });
-  }, [songs]);
+    useEffect(() => {
+        if (songs.length > 0 && contestantNumber > 0 && !songsRandomized) {
+            const selectedSongs = getRandomSongs(songs, contestantNumber);
+            setSongsState(selectedSongs);
+            dispatch(setSongs(selectedSongs));
+            setSongsRandomized(true);
+        }
+    }, [songs, contestantNumber, dispatch]);
 
-  return (
-    <div>
-      <h1>Bracket Page</h1>
-      {selectedPlaylist ? (
+    return (
         <div>
-          <h2>{selectedPlaylist.name}</h2>
-          <ul>
-            {songs.map((song) => (
-              <li key={song.id}>{song.name} by {song.artists.map(artist => artist.name).join(', ')}</li>
-            ))}
-          </ul>
+        <h1>Bracket Page</h1>
+        {selectedPlaylist ? (
+            <div>
+            <h2>{selectedPlaylist.name}</h2>
+            <ul>
+                {songs.map((song) => (
+                <li key={song.id}>{song.name} by {song.artists.map(artist => artist.name).join(', ')}</li>
+                ))}
+            </ul>
+            </div>
+        ) : (
+            <p>No playlist selected</p>
+        )}
         </div>
-      ) : (
-        <p>No playlist selected</p>
-      )}
-    </div>
-  );
+    );
 };
 
 export default BracketPage;

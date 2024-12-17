b2ad9b22b1b108b548fa84555e858b50de9caccd
sentinelEXE <sngfshr@gmail.com>
2024-12-17 17:06:03 -0600

    Refactor strings, improve styles on StartPage

--- src/components/pages/StartPage.tsx
+++ src/components/pages/StartPage.tsx
@@ -6,13 +6,14 @@ import { Playlist } from '../../types/types';
 import { setSelectedPlaylist, setContestantNumber, setBracketTitle } from '../../store/store';
 import { fetchPlaylists } from '../../api/fetch-playlists';
 import { useAsync } from '../../hooks/use-async';
+import { STRINGS } from '../../types/strings';
 import '../../styles/pages/StartPage.css';
 
 export const StartPage: React.FC = () => {
     const accessToken = localStorage.getItem('access_token');
     const dispatch = useDispatch();
     const [title, setTitle] = useState<string>('');
-    const [contestantNumber, setContestantNumberState] = useState<number>(2);
+    const [contestantNumber, setContestantNumberState] = useState<number>(0);
     const [playlists, setPlaylists] = useState<any[]>([]);
     const [selectedPlaylist, setSelectedPlaylistState] = useState<Playlist | null>();
 
@@ -65,37 +66,39 @@ export const StartPage: React.FC = () => {
 
     return (
         <div className='StartPage'>
-            <h1>Music Bracket</h1>
-            <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Bracket title'/>
-            <br/>
-            <label htmlFor="contestantNumber">Select number of songs:</label>
-            <select 
-                value={contestantNumber} 
-                onChange={(e) => setContestantNumberState(Number(e.target.value))}
-            >
-                {[2, 4, 8, 16, 32, 64, 128].map(num => (
-                    <option key={num} value={num}>{num}</option>
-                ))}
-            </select>
-            <br/>
-            <label htmlFor="playlist">Select a playlist:</label>
-            <select 
-                id="playlist"
-                value={selectedPlaylist?.id || ""} 
-                onChange={(e) => {
-                    const playlist = playlists.find(p => p.id === e.target.value);
-                    if (playlist) handlePlaylistChange(playlist);
-                  }}
-            >
-                <option value="" disabled>Select a playlist</option>
-                {playlists.map(playlist => (
-                    <option key={playlist.id} value={playlist.id}>{playlist.name}</option>
-                ))}
-            </select>
-            <br/>
-            {errorMessage && <p>{errorMessage}</p>}
-            <br/>
-            <button disabled={errorMessage !== ""} onClick={handleGenerateBracket}>Generate Bracket</button>
+            <h1>{STRINGS.TITLE}</h1>
+            <p className='instructions'>{STRINGS.INSTRUCTIONS}</p>
+            <div className='inputs'>
+                <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Bracket title'/>
+                <br/>
+                <select 
+                    value={contestantNumber || ""} 
+                    onChange={(e) => setContestantNumberState(Number(e.target.value))}
+                >
+                    <option value="" disabled>{STRINGS.SELECT_NUMBER_OF_SONGS}</option>
+                    {[2, 4, 8, 16, 32, 64, 128].map(num => (
+                        <option key={num} value={num}>{num}</option>
+                    ))}
+                </select>
+                <br/>
+                <select 
+                    id="playlist"
+                    value={selectedPlaylist?.id || ""} 
+                    onChange={(e) => {
+                        const playlist = playlists.find(p => p.id === e.target.value);
+                        if (playlist) handlePlaylistChange(playlist);
+                    }}
+                >
+                    <option value="" disabled>{STRINGS.SELECT_PLAYLIST}</option>
+                    {playlists.map(playlist => (
+                        <option key={playlist.id} value={playlist.id}>{playlist.name}</option>
+                    ))}
+                </select>
+                <br/>
+                {errorMessage && <p>{errorMessage}</p>}
+                <br/>
+                <button disabled={errorMessage !== ""} onClick={handleGenerateBracket}>{STRINGS.GENERATE_BRACKET}</button>
+            </div>
         </div>
     )
 };

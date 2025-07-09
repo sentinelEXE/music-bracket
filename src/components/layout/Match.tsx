cd30c10b5020c248c4f24033773e2470dbc4f527
SentinelEXE <sngfshr@gmail.com>
2025-07-08 21:12:05 -0500

    make it prettier

--- src/components/layout/Match.tsx
+++ src/components/layout/Match.tsx
@@ -11,17 +11,19 @@ interface MatchProps {
 export const Match: React.FC<MatchProps> = ({ match }) => {
     const isReady = useMemo(() => match.songs[0] && match.songs[1], [match.songs]);
     const matchState = match.matchState;
+    const song0Wins = matchState === MatchState.Song0Wins;
+    const song1Wins = matchState === MatchState.Song1Wins;
 
     const matchContent = (
         <div className="match-box">
-          <div>
-            {matchState === MatchState.Song0Wins && <span className="checkmark">✔ </span>} {match.songs[0]?.name ?? 'TBD'}
+          <div className={song0Wins ? "winner" : ""}>
+            {song0Wins && <span className="checkmark">✔ </span>} {match.songs[0]?.name ?? 'TBD'}
           </div>
           <div>
             vs
           </div>
-          <div>
-            {matchState === MatchState.Song1Wins && <span className="checkmark">✔ </span>}{match.songs[1]?.name ?? 'TBD'}
+          <div className={song1Wins ? "winner" : ""}>
+            {song1Wins && <span className="checkmark">✔ </span>}{match.songs[1]?.name ?? 'TBD'}
           </div>
         </div>
       );
